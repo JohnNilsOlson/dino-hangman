55ab7b69855e9f62053b2721e33ce95944093934
JohnNils Olson <johnnils@gmail.com>
2020-07-01 11:09:43 -0700

    add fetch and dino name display
    Co-authored-by: Erich Richter<erich.richter@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 
-$(document).ready(function() {
+function getElements(response) {
+  if (response) {
 
+    const dino = response[0][0];
+    // const splicedResponse;
+
+    // splicedResponse = repsonse.pop();
+    // dino = splicedResponse[0][1];
+
+    $('#dino-output').text(dino);
+    $('#error-output').text("");
+  } else {
+    $('#dino-output').text("");
+    $('#error-output').text(`There was an error: ${response.message}`);
+  }
+}
+
+$(document).ready(function() {
+  $('#generate').click(function() {
+    fetch(`http://dinoipsum.herokuapp.com/api/?format=json`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+      .then(function(jsonifiedResponse) {
+        getElements(jsonifiedResponse);
+      });
+  });
 });
